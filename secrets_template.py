e757fe2e7fded2e1d96b94a66395011270b83617
Steven <stevencoding@gmail.com>
2014-05-25 16:22:41 -0700

    create the template for secrets.py

---
+++ secrets_template.py
@@ -0,0 +1,10 @@
+# shall rename this file as secrets.py, and fill in the info below represented by xxx
+
+from linkedin.utils import * 
+
+secret = {'API_KEY':"xxx",
+			'Secret_Key':"xxx",
+			'OAUT':"xxx",
+			'OAUS':"xxx",
+            'Group_ID':xxx}
+            
